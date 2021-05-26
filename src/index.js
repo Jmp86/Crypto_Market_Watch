10434e33ffd7ff23a539265089b71b8c5780f808
Justin <jmp86@yahoo.com>
2021-05-25 16:36:39 -1000

    background, tiles, more info

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 const BASE_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
 
 const mainPage = document.getElementById('main')
-const cryptoList = document.querySelector('cryptolist')
+const selectedCrypto = document.getElementById('crypto')
 
 
 fetch(BASE_URL)
 .then(res => res.json())
-.then(
-  (crypto) => {
-  crypto.forEach((coin) => {
+.then((crypto) => {
+    crypto.forEach((coin) => {
     createTile(coin)
-    console.log(coin)
   })
 })
 
@@ -20,6 +18,8 @@ function createTile(tile) {
     const pTag = document.createElement('p')
     const btnTag = document.createElement('button')
     const imgTag = document.createElement('img')
+    newTile.classList.add('tile')
+    imgTag.classList.add('logo')
     btnTag.id = tile.id
     imgTag.src = tile.image
     pTag.innerText = `Current Value : $${tile.current_price}`
@@ -27,4 +27,42 @@ function createTile(tile) {
     hTag.innerText = tile.name
     newTile.append(hTag, imgTag, pTag, btnTag)
     mainPage.appendChild(newTile)
+    btnTag.addEventListener('click', (e) => {
+      cryptoSelect(tile)
+    })
   }
+
+  function cryptoSelect(select) {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then((crypto) => {
+      moreInfo(select)
+      // console.log(select)
+    })
+  
+}
+
+function moreInfo (info) {
+  mainPage.innerHTML = ''
+  const singleTile = document.createElement("div")
+  const hTag = document.createElement('h2')
+  const pTag = document.createElement('p')
+  const imgTag = document.createElement('img')
+  singleTile.classList.add('single')
+  imgTag.classList.add('logo')
+  imgTag.src = info.image
+  pTag.innerText = `Current Value : $${info.current_price}`
+  hTag.innerText = info.name
+  for (const [key, value] of Object.entries(info)) {
+    listInfo(`${key}: ${value}`);
+  }
+  singleTile.append(hTag, imgTag, pTag, selectedCrypto)
+  mainPage.appendChild(singleTile)
+}
+
+function listInfo (list){
+  const li = document.createElement("li")
+  li.classList.add('details')
+  li.innerText = list
+  selectedCrypto.appendChild(li)
+}
